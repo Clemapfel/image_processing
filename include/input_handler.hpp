17ce3547cd9cfea54bcc3259e78db7e8f909e2fa
clem <clemens.cords@aol.com>
2021-07-26 21:56:21 +0200

    input handler documentation first draft done

--- include/input_handler.hpp
+++ include/input_handler.hpp
@@ -10,6 +10,7 @@
 
 #include <SFML/System/Time.hpp>
 #include <SFML/Window/Event.hpp>
+
 #include <key_id.hpp>
 #include <vector.hpp>
 
@@ -17,20 +18,43 @@ namespace crisp
 {
     class RenderWindow;
 
+    // @brief static handler that allows accesing the keyboard/mouse state
     class InputHandler
     {
         friend class RenderWindow;
 
         public:
+            // @brief InputHandler cannot be constructed, all access happens through static functions
             InputHandler() = delete;
 
-            static bool has_state_changed(KeyID key_id);
+            // @brief does the current state show that the key is pressed?
+            // @param key_id: id of key
+            // @returns true if down, false otherwise
             static bool is_key_down(KeyID key_id);
+
+            // @brief is the key state this cycle different than last cycle
+            // @param key_id: id of key
+            // @returns true if state is different, false otherwise
+            static bool has_state_changed(KeyID key_id);
+
+            // @brief is key down this cycle, not down last cycle
+            // @param key_id: id of key
+            // @returns true if key is down this cycle and not down last cycle, false otherwise
             static bool was_key_pressed(KeyID key_id);
+
+            // @brief is key not down this cycle, down last cycle
+            // @param key_id: id of key
+            // @returns true if key is up this cycle and down last cycle, false otherwise
             static bool was_key_released(KeyID key_id);
+
+            // @brief how long was the key in a pressed state
+            // @param key_id: id of key
+            // @returns time since the key was first pressed
+            // @note the duration only updates after each render cycles because it is tied to the operating systems window updating
             static sf::Time get_hold_duration(KeyID key_id);
 
-            // @returns absolute position relative to window origin
+            // @brief get position of mouse cursor relative to the windows origin
+            // @returns {0,0} if the cursor is in the top left of the window, {resolution.x, resolution.y} if in the bottom right, outside those bounds if the cursor is outside of the window
             static Vector2f cursor_position();
 
         protected:
