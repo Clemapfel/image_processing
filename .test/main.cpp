bfb487244e65b63e5abac6afc7923cc8de610993
clem <clemens.cords@aol.com>
2021-08-10 15:41:31 +0200

    dc term at n*m/2 + m/2

--- .test/main.cpp
+++ .test/main.cpp
@@ -30,6 +30,146 @@ int main()
     RenderWindow window;
     window.create(image_in.get_size().x * 2, image_in.get_size().y * 2);
 
+    std::cout << "image: " << image_in.get_size().x << "x" << image_in.get_size().y << std::endl;
+
+    size_t n = image_in.get_size().x * 2;
+    size_t m = image_in.get_size().y * 2;
+
+    auto* in = fftwl_alloc_complex(n*m);
+    auto* out = fftwl_alloc_complex(n*m);
+
+    {
+    size_t i = 0;
+    for (long x = 0; x < n; ++x)
+        for (long y = 0; y < m; ++y, ++i)
+        {
+            in[i][0] = image_in.get_pixel_or_padding(x, y) * pow(-1, x + y);
+            in[i][1] = 0;
+        }
+    }
+
+    auto plan_to = fftwl_plan_dft_2d(n, m, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+    auto plan_from = fftwl_plan_dft_2d(n, m, out, in, FFTW_BACKWARD, FFTW_ESTIMATE);
+
+    fftwl_execute(plan_to);
+
+    // output
+    GrayScaleImage image_out;
+    image_out.create(n, m, 1);
+
+    const auto hn = floor(n / 2.f), hm = floor(m / 2.f);
+    size_t i = 0;
+
+    double mean = 0;
+    double min = std::numeric_limits<double>::max();
+    double max = std::numeric_limits<double>::min();
+    size_t max_i = 0;
+
+    UniformNoise noise;
+    noise.set_interval(-1, 1);
+
+    for (long x = 0; x < n; ++x)
+    {
+        for (long y = 0; y < m; ++y, ++i)
+        {
+            auto f = std::complex<double>(out[i][0], out[i][1]);
+
+            double spectrum = abs(f);
+            double angle = arg(f);
+
+            f = std::polar(spectrum, angle);
+            out[i][0] = f.real();
+            out[i][1] = f.imag();
+
+            double value = log(1 + spectrum);
+            min = std::min(value, min);
+
+            if (value > max)
+                max_i = i;
+
+            max = std::max(value, max);
+        }
+    }
+
+    std::cout << max_i << std::endl;
+    std::cout << n*m/2 + m/2 << std::endl;
+
+    // normalize intensities
+    i = 0;
+    for (long x = 0; x < n; ++x)
+    {
+        for (long y = 0; y < m; ++y, ++i)
+        {
+            auto f = std::complex<double>(out[i][0], out[i][1]);
+            double value = log(1 + abs(f));
+
+            if (min < 0)
+            {
+                value += min;
+                value /= max;
+            }
+            else
+            {
+                value -= min;
+                value /= (max + min);
+            }
+
+            image_out(x, y) = value;
+        }
+    }
+
+    Sprite sprite;
+    sprite.load_from(image_out);
+
+    image_in.create(n/2, m/2, 1);
+
+    fftwl_execute(plan_from);
+    i = 0;
+    for (long x = 0; x < n; ++x)
+        for (long y = 0; y < m; ++y, ++i)
+            if (x < n/2 and y < m/2)
+            {
+                image_in(x, y) = in[i][0] / (m*n) * pow(-1, x+y);
+            }
+
+    bool which = false;
+    while (window.is_open())
+    {
+        window.update();
+
+        if (InputHandler::was_key_pressed(SPACE))
+        {
+            which = not which;
+            sprite.load_from(which ? image_in : image_out);
+        }
+
+        window.clear();
+        window.draw(sprite);
+        window.display();
+    }
+
+    fftwl_destroy_plan(plan_to);
+    fftwl_destroy_plan(plan_from);
+    fftwl_free(in);
+    fftwl_free(out);
+}
+
+/*
+template<typename T>
+T project(T lower_bound, T upper_bound, T value)
+{
+    return value * fabs(lower_bound - upper_bound) + std::min(lower_bound, upper_bound);
+}
+
+int main()
+{
+    GrayScaleImage image_in;
+    image_in.create_from_file("/home/clem/Workspace/image_processing/docs/opal_clean.png");
+    image_in.set_padding_type(GrayScaleImage::PaddingType::ONE);
+
+    RenderWindow window;
+    window.create(image_in.get_size().x * 2, image_in.get_size().y * 2);
+
     size_t m = image_in.get_size().x * 2;
     size_t n = image_in.get_size().y * 2;
 
